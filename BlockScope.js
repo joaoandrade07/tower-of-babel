44f64235ca6eb05aca2eb779f13338dcaf8d10d3
João Andrade <114431775+joaoandrade07@users.noreply.github.com>
2023-03-06 22:45:49 -0300

    Add files via upload

---
+++ BlockScope.js
@@ -0,0 +1,21 @@
+// Mudar o nome do arquivo para solution.js para funcionar.
+
+'use strict';
+var a = 5;
+const b = process.argv[2];
+
+if (a === 5) {
+    let c = 4;
+    console.log(c);  // 4
+
+    let b = 8;
+    console.log(b);
+}
+
+console.log(a); // 5
+console.log(b);
+try {
+    c = 1000;
+} catch (e) {
+    console.log(e);
+}
